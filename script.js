24ac0a1a2db9e907e0bec619033f71a417cb5226
exctgum <141287168+exctgum@users.noreply.github.com>
2023-08-03 22:31:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,46 @@ function updateTime() {
     const now = new Date();
     const clockElement = document.querySelector('#realtime-clock');
     clockElement.textContent = now.toLocaleString();
+    // Daftar proyek dengan data yang akan ditampilkan
+const projects = [
+  {
+    title: "Proyek 1",
+    description: "Deskripsi proyek 1",
+    previewImage: "project1_preview.jpg",
+  },
+  {
+    title: "Proyek 2",
+    description: "Deskripsi proyek 2",
+    previewImage: "project2_preview.jpg",
+  },
+  // Tambahkan proyek lainnya di sini
+];
+
+// Fungsi untuk menampilkan data proyek pada preview
+function showProjectPreview(index) {
+  const projectTitle = document.querySelector("#project-title");
+  const projectDescription = document.querySelector("#project-description");
+  const projectPreviewImage = document.querySelector("#project-preview-image");
+
+  projectTitle.textContent = projects[index].title;
+  projectDescription.textContent = projects[index].description;
+  projectPreviewImage.src = projects[index].previewImage;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ... kode lainnya ...
+
+  // Mendapatkan daftar tombol preview
+  const previewButtons = document.querySelectorAll(".preview-button");
+
+  // Menambahkan event listener ke setiap tombol preview
+  previewButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      showProjectPreview(index);
+    });
+  });
+});
+
 }
 
 setInterval(updateTime, 1000);
